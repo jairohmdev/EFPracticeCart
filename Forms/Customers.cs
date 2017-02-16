aa4bb2150393d490c564f13f6e0e3bb2ed16d9e1
jairohmdev <jairoh.tuada@mllrdev.com>
2017-02-16 18:08:44 +0800

    Added Manage Customer functionality (Update&Delete)

--- Forms/Customers.cs
+++ Forms/Customers.cs
@@ -84,5 +84,58 @@ namespace Forms
             return _repository.GetAll().Count;
         }
 
+        private void RefreshButton_Click(object sender, EventArgs e)
+        {
+            DetermineDisplayDirection("");
+        }
+
+        private void CustomersListView_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            var row = CustomersListView.FocusedItem.Index;
+
+            IdLabel.Text = CustomersListView.Items[row].SubItems[0].Text;
+            NameLabel.Text = CustomersListView.Items[row].SubItems[1].Text;
+            AddressLabel.Text = CustomersListView.Items[row].SubItems[2].Text;
+            CityLabel.Text = CustomersListView.Items[row].SubItems[3].Text;
+            ZipLabel.Text = CustomersListView.Items[row].SubItems[4].Text;
+            StateLabel.Text = CustomersListView.Items[row].SubItems[5].Text;
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            if (_repository.Update(GetSelectedCustomer()))
+            {
+                MessageBox.Show("Customer updated!");
+            }
+            else
+            {
+                MessageBox.Show("Error: Unable to update customer.");
+            }
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            if (_repository.Delete(GetSelectedCustomer().Id))
+            {
+                MessageBox.Show("Customer Deleted!");
+            }
+            else
+            {
+                MessageBox.Show("Error: Unable to delete customer.");
+            }
+        }
+        private Customer GetSelectedCustomer()
+        {
+            return new Customer()
+            {
+                Id = int.Parse(IdLabel.Text),
+                Name = NameLabel.Text,
+                Address = AddressLabel.Text,
+                City = CityLabel.Text,
+                Zip = ZipLabel.Text,
+                State = StateLabel.Text
+            };
+        }
+
     }
 }
